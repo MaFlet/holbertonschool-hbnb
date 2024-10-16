d1a73d21163a530020666ef83df9c36ee12bbe7e
Vlad <vgozhyi@gmail.com>
2024-10-16 09:00:47 +0000

    Trying to get imports working.

--- app/models/user.py
+++ app/models/user.py
@@ -2,7 +2,7 @@ import uuid
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self, id, first_name, last_name, email, is_admin = False):
+    def __init__(self, first_name, last_name, email, is_admin = False):
         self.id = str(uuid.uuid4())
         self.first_name = first_name
         self.last_name = last_name
@@ -11,10 +11,14 @@ class BaseModel:
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
+class User(BaseModel):
+    """"""
+    def __init__(self, first_name, last_name, email, is_admin=False):
+        super().__init__(first_name, last_name, email, is_admin)
+
 if __name__ == "__main__":
-    User = BaseModel()
     def test_user_creation():
-        user = User(first_name="John", last_name="Doe", email="john.doe@example.com")
+        user =  BaseModel(first_name="John", last_name="Doe", email="john.doe@example.com")
         assert user.first_name == "John"
         assert user.last_name == "Doe"
         assert user.email == "john.doe@example.com"
