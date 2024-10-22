db484e81a1ea393f32b11791fb62f389bf17198b
MaFlet <mfletcher2808@hotmail.com>
2024-10-22 22:03:52 +1100

    Modified places

--- app/models/place.py
+++ app/models/place.py
@@ -3,17 +3,15 @@ from datetime import datetime
 
 class Place():
     """ """
-    def __init__(self, title, description, price, latitude, longitude, owner, owner_id, amenities) -> None:
+    def __init__(self, title, description, price, latitude, longitude, owner_id, amenities=None):
         self.id = str(uuid.uuid4())
         self.title = title
         self.description = description
         self.price = price
         self.latitude = latitude
         self.longtitude = longitude
-        self.owner = owner
         self.owner_id = owner_id
-        self.created_at = datetime.now
-        self.updated_at = datetime.now
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
         self.reviews = []
-        self.amenities = amenities
-        pass
+        self.amenities = amenities or []
