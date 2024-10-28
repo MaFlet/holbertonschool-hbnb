2b384726b67b689e95b811680eadd3e28b2704f5
Vlad <vgozhyi@gmail.com>
2024-10-28 20:36:24 +1100

    Bare-bones User Unittest

--- app/models/user.py
+++ app/models/user.py
@@ -1,5 +1,6 @@
 import uuid
 from datetime import datetime
+import re
 
 class User():
     def __init__(self, first_name, last_name, email, is_admin=False) -> None:
@@ -12,3 +13,71 @@ class User():
         self.updated_at = datetime.now
         self.places = []
         pass
+
+    @property
+    def first_name(self):
+        """"""
+        return self._first_name
+
+    @first_name.setter
+    def first_name(self, val):
+        """"""
+        if isinstance(val, str):
+            self._first_name = val
+        else:
+            raise ValueError(
+                "First name must be a valid string object"
+            )
+        
+    @property
+    def last_name(self):
+        """"""
+        return self._last_name
+
+    @last_name.setter
+    def last_name(self, val):
+        """"""
+        if isinstance(val, str):
+            self._last_name = val
+        else:
+            raise ValueError(
+                "First name must be a valid string object"
+            )
+
+    @property
+    def email(self):
+        """"""
+        return self._email
+
+    @email.setter
+    def email(self, val):
+        """"""
+        if isinstance(val, str) and re.match(
+            r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', val):
+            self._email = val
+        else:
+            raise ValueError("Invalid email address entered.")
+    @property
+    def is_admin(self):
+        """"""
+        return self._is_admin
+
+    @is_admin.setter
+    def is_admin(self, val):
+        """"""
+        if isinstance(val, bool):
+            self._is_admin = val
+        else:
+            raise ValueError("is_admin must be a bool.")
+
+    def add_place(self, place):
+        """"""
+        # Possibly necessary to do checks to see if place is a valid object
+    
+    def add_review(self, review):
+        """"""
+        # Again, validate review
+
+    #def to_JSON(self):
+    # Yeah... I figured this out pretty late
+    # Anyway, up to the team whether or not we wanna do it here or in the API
